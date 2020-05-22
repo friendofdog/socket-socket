999e4f5646b40877e69df33ca696b55ea3bcabf2
Kevin Kee <kevinkee9@gmail.com>
2020-05-22 07:44:55 +0000

    server: make client socket global variable
    Make client socket global variable. Server was already global variable
    and remains so.
    
    Client socket creation and header construction are handled through
    dedicated functions. Were previously handled by listen().
    
    Rename setup() -> create_server(), as this better describes the function.

--- server.py
+++ server.py
@@ -1,23 +1,33 @@
 import socket
 from config import HEADERSIZE
 
-def setup():
+def create_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((socket.gethostname(),1234)) # bind IP address on port 1234
     s.listen(5)                         # queue limit for incoming connections
     return s
 
-def listen(s):
-    clientsocket, address = s.accept()  # if anyone connects, will accept
-                                        # these are socket and addr of client
+def create_clientsocket():
+    c, address = s.accept()     # if anyone connects, will accept these are
+                                # socket and addr of client
     print(f"Connection from {address} established")
+    return c
 
-    msg = "Welcome to the server"
-    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+def make_header(msg):
+    return f"{len(msg):<{HEADERSIZE}}"
 
-    clientsocket.send(bytes(msg, 'utf-8'))
 
-server = setup()
+    c.send(bytes(msg, 'utf-8'))
+
+def listen():
+    msg = "Welcome to the server. This is a longer message."
+    header = make_header(msg)
+    msg = f"{header}" + msg
+
+    c.send(bytes(msg, 'utf-8'))
+
+s = create_server()
+c = create_clientsocket()
 
 while True:
-    listen(server)
+    listen()
